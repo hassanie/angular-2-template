563da22f81a2e5a421acad28612fe5d99a2c8835
Hassan Latif <latif.hassan1@gmail.com>
2017-05-15 04:43:50 -0400

    first

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,26 @@
 import {Component} from 'angular2/core';
+import {config} from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from './playlist.component';
+
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html'
+    directives: [PlaylistComponent]
 })
 
-export class AppComponent {}
+
+export class AppComponent {
+
+  mainHeading=config.MAIN_HEADING;
+  videos:Array<Video>;
+
+  constructor(){
+    this.videos= [
+      new Video(1,"installing Django", "qgGIqRFvFFK", "How to install Django"),
+      new Video(2,"survive Django", "Fgwy-udtyLs", "Rip")
+    ]
+  }
+
+}
---
+++ app/ts/config.service.ts
@@ -0,0 +1,4 @@
+export class config{
+
+  static MAIN_HEADING: string ="My Favorite Videos"
+}
